45c4363a913a433955c71caa5b58a2fc34562814
Z3ldaf <falliprandi@tutanota.com>
2022-04-21 17:25:22 +0200

    adding menu
    change skybox and brick material.
    And i added a menu

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -7,27 +7,27 @@ using UnityEngine.Events;
 public class Brick : MonoBehaviour
 {
     public UnityEvent<int> onDestroyed;
-    
+
     public int PointValue;
 
     void Start()
     {
-        var renderer = GetComponentInChildren<Renderer>();
+        var renderer = GetComponentInChildren<MeshRenderer>();
 
         MaterialPropertyBlock block = new MaterialPropertyBlock();
         switch (PointValue)
         {
             case 1 :
-                block.SetColor("_BaseColor", Color.green);
+                renderer.material.color = Color.green;
                 break;
             case 2:
-                block.SetColor("_BaseColor", Color.yellow);
+                renderer.material.color = Color.yellow;
                 break;
             case 5:
-                block.SetColor("_BaseColor", Color.blue);
+                renderer.material.color = Color.blue;
                 break;
             default:
-                block.SetColor("_BaseColor", Color.red);
+                renderer.material.color = Color.red;
                 break;
         }
         renderer.SetPropertyBlock(block);
@@ -36,7 +36,7 @@ public class Brick : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         onDestroyed.Invoke(PointValue);
-        
+
         //slight delay to be sure the ball have time to bounce
         Destroy(gameObject, 0.2f);
     }
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -7,30 +7,30 @@ using UnityEngine.UI;
 public class MainManager : MonoBehaviour
 {
     public Brick BrickPrefab;
-    public int LineCount = 6;
+    public int LineCount = 12;
     public Rigidbody Ball;
 
     public Text ScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        const float step = 0.7f;
+        int perLine = Mathf.FloorToInt(10.0f / step);
+
+        int[] pointCountArray = new [] {1,1,2,2,3,3,4,4,5,5,10,10};
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
             {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                Vector3 position = new Vector3(-4.5f + step * x, 2.5f + i * 0.3f, 0);
                 var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
@@ -57,7 +57,7 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                SceneManager.LoadScene("Menu");
             }
         }
     }
